b73fe105be3c1b9496e51f5e9a50aa9803eb9644
Devangam Sajja Rajesh <104376917+Rajeshds20@users.noreply.github.com>
2023-06-04 22:32:00 +0530

    Add files via upload

--- src/NoiseTracker.js
+++ src/NoiseTracker.js
@@ -1,15 +1,19 @@
 import React, { useEffect, useState, useRef } from 'react';
+import './App.css';
 
 const NoiseTracker = () => {
     const [noiseLevel, setNoiseLevel] = useState(0);
-    const canvasRef = useRef(null);
+    const waveCanvasRef = useRef(null);
     const animationIdRef = useRef(null);
+    const [threshold, setThreshold] = useState(20);
+    const [alert, setAlert] = useState(false);
+    const [averageNoiseLevel, setAverageNoiseLevel] = useState(0);
 
     useEffect(() => {
         let audioContext;
         let analyser;
         let mediaStreamSource;
-        let canvasContext;
+        let waveCanvasContext;
 
         // Function to start tracking noise levels
         const startNoiseTracking = async () => {
@@ -20,53 +24,63 @@ const NoiseTracker = () => {
                 analyser = audioContext.createAnalyser();
                 mediaStreamSource = audioContext.createMediaStreamSource(mediaStream);
                 mediaStreamSource.connect(analyser);
-                setupCanvas();
+                analyser.fftSize = 2048; // Set the FFT size for frequency analysis
+                setupWaveCanvas();
                 updateNoiseLevel();
             } catch (error) {
                 console.error('Error accessing microphone:', error);
+                alert('Accessing microphone failed. Please check your browser settings.');
             }
         };
 
         // Function to set up the canvas for visualizing the audio wave
-        const setupCanvas = () => {
-            const canvas = canvasRef.current;
-            canvasContext = canvas.getContext('2d');
-            canvasContext.lineWidth = 2;
-            canvasContext.strokeStyle = 'blue';
+        const setupWaveCanvas = () => {
+            const canvas = waveCanvasRef.current;
+            waveCanvasContext = canvas.getContext('2d');
+            waveCanvasContext.lineWidth = 2;
+            waveCanvasContext.strokeStyle = 'blue';
         };
 
         // Function to update the noise level and draw the audio wave
         const updateNoiseLevel = () => {
+            if (noiseLevel > threshold) {
+                setAlert(true);
+            }
             const dataArray = new Uint8Array(analyser.fftSize);
             analyser.getByteTimeDomainData(dataArray);
             const average = [...dataArray].reduce((acc, val) => acc + val, 0) / dataArray.length;
-            const noiseLevel = Math.max(0, average - 127);
-            setNoiseLevel(noiseLevel);
+            const nlevel = Math.max(0, average - 127);
+            setNoiseLevel(nlevel);
             drawWave(dataArray);
+            // Calculate average noise level
+            const sum = dataArray.reduce((acc, val) => acc + val, 0);
+            const averageN = sum / dataArray.length;
+            setAverageNoiseLevel(averageN);
+
+            // Request next animation frame
             animationIdRef.current = requestAnimationFrame(updateNoiseLevel);
         };
-
         // Function to draw the audio wave on the canvas
         const drawWave = (dataArray) => {
-            const canvas = canvasRef.current;
+            const canvas = waveCanvasRef.current;
             const width = canvas.width;
             const height = canvas.height;
-            canvasContext.clearRect(0, 0, width, height);
-            canvasContext.beginPath();
+            waveCanvasContext.clearRect(0, 0, width, height);
+            waveCanvasContext.beginPath();
             const sliceWidth = (width * 1.0) / dataArray.length;
             let x = 0;
             for (let i = 0; i < dataArray.length; i++) {
                 const v = dataArray[i] / 128.0;
                 const y = (v * height) / 2;
                 if (i === 0) {
-                    canvasContext.moveTo(x, y);
+                    waveCanvasContext.moveTo(x, y);
                 } else {
-                    canvasContext.lineTo(x, y);
+                    waveCanvasContext.lineTo(x, y);
                 }
                 x += sliceWidth;
             }
-            canvasContext.lineTo(width, height / 2);
-            canvasContext.stroke();
+            waveCanvasContext.lineTo(width, height / 2);
+            waveCanvasContext.stroke();
         };
 
         startNoiseTracking();
@@ -79,11 +93,49 @@ const NoiseTracker = () => {
         };
     }, []);
 
+    const handleExportData = () => {
+        const dataToExport = {
+            noiseLevel,
+        };
+        const dataBlob = new Blob([JSON.stringify(dataToExport)], { type: 'application/json' });
+        const url = URL.createObjectURL(dataBlob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'noise_data.json';
+        link.click();
+        URL.revokeObjectURL(url);
+    };
+
     return (
-        <div>
-            <h1 style={{ color: "blueviolet" }} className="head">Simple Noice Tracker App</h1>
-            <h2>Noise Level: {noiseLevel}</h2>
-            <canvas ref={canvasRef} width={400} height={100} />
+        <div className="container">
+            <h1 className="head">Simple Noise Tracker App</h1>
+            <div className="info-section">
+                <div className="noise-level">
+                    <h2 className={`noise-level-value ${noiseLevel > threshold ? 'high' : 'normal'}`}>
+                        Noise Level: {Math.round(noiseLevel)}
+                    </h2>
+                    <h2>Average Noise Level: {Math.round(averageNoiseLevel)}</h2>
+                    <label className="threshold-label">Threshold:</label>
+                    <input
+                        type="number"
+                        className="threshold-input"
+                        value={threshold}
+                        onChange={(e) => setThreshold(parseInt(e.target.value))}
+                    />
+                </div>
+                <button className="export-button" onClick={handleExportData}>
+                    Export Data
+                </button>
+            </div>
+            <div className="alert-section">
+                <h2 className={`alert ${alert ? 'show' : 'hide'}`}>Noise level is too high!</h2>
+            </div>
+            <div className="canvas-section">
+                <div className="wave-canvas-container">
+                    <h2>Wave Visualization</h2>
+                    <canvas ref={waveCanvasRef} className="wave-canvas" />
+                </div>
+            </div>
         </div>
     );
 };
